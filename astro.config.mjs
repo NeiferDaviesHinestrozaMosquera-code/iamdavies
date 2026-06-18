8711e9d4676ffc454890064157340c7ae26990ce
unknown <neiferdaviesmosq@outlook.com>
2026-06-18 18:32:26 -0500

    v0.0.1.1 new

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   site: process.env.PUBLIC_SITE_URL || 'https://iamdavies.com',
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
 });
